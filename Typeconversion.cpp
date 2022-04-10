598b62b5382ed5b3b8f83518e00c4488bc15f16a
Shweta <ranjanshweta2203@gmail.com>
2022-04-10 15:34:46 +0530

    notes

--- Typeconversion.cpp
+++ Typeconversion.cpp
@@ -33,6 +33,20 @@ class distance1{
     }
         
 };
+class circle{
+    float x;
+    public:
+    void get(){
+        float radius;
+        cin>>radius;
+        x=radius;
+    }
+    operator float(){
+        float ar;
+        ar=3.142*x*x;
+        return ar;
+    }
+};
 int main()
 { 
 //basic to basic  
@@ -48,10 +62,19 @@ cout<<x<<endl; //3
   t1=duration;
  t1.display();
  //class to basic
-distance1 d1;
-float m1;
-cin>>m1;
-d1=m1;
-d1.display();
+ //overloaded casting operator could be used to convert a class type data to basic type(know as conversion function)
+ //the casting operator should satisfy following condition:-
+ //it must be a class member , no return type , no arguments
+ //Syntax:- operator typename(){ //function statement}
+ circle c1;
+ float area;
+ c1.get();
+ area=c1;
+ cout<<area<<endl;
+// distance1 d1;
+// float m1;
+// cin>>m1;
+// d1=m1;
+// d1.display();
  return 0;
 }
